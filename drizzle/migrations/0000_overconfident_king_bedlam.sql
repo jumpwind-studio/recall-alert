222b0e8cdc0a994e78f25628ad25a481d02784b1
timredd <timredd13@gmail.com>
2024-11-24 00:30:31 -0500

    Add initial migrations

---
+++ drizzle/migrations/0000_overconfident_king_bedlam.sql
@@ -0,0 +1,13 @@
+CREATE TABLE `recalls` (
+	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
+	`date` integer,
+	`linkHref` text NOT NULL,
+	`linkText` text NOT NULL,
+	`product` text NOT NULL,
+	`category` text NOT NULL,
+	`reason` text NOT NULL,
+	`company` text NOT NULL,
+	`createdAt` integer DEFAULT (strftime('%s', 'now')) NOT NULL,
+	`updatedAt` integer DEFAULT (strftime('%s', 'now')) NOT NULL,
+	`deleted_at` integer
+);
