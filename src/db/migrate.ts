b1ed8e85ea76163bc6f93bd6ae108094aaa50c10
timredd <timredd13@gmail.com>
2024-11-25 11:06:11 -0500

    Clean up unused migrate file

--- src/db/migrate.ts
+++ src/db/migrate.ts
@@ -1,17 +1,13 @@
 import { migrate as drizzleMigrate } from 'drizzle-orm/d1/migrator';
 import { useDatabase } from './client';
 
-export const migrate = async () => {
-  const db = useDatabase();
+export const migrate = async (env: Env) => {
+  const db = useDatabase(env.DB);
 
   try {
-    await drizzleMigrate(db, {
-      migrationsFolder: './migrations',
-    });
+    await drizzleMigrate(db, { migrationsFolder: 'drizzle/migrations' });
   } catch (err) {
     console.error(err);
     process.exit(1);
   }
 };
-
-migrate();
