dd12c694d4e5517615d3cb9e303294c17161560e
timredd <timredd13@gmail.com>
2024-11-25 22:04:58 -0500

    Add sources and posts table

--- src/db/client.ts
+++ src/db/client.ts
@@ -1,15 +1,16 @@
-import { recallsTable } from '@/db/schemas.sql';
+import { postsTable, recallsTable, sourcesTable } from '@/db/schemas.sql';
 import type { DrizzleConfig } from 'drizzle-orm';
 import { drizzle } from 'drizzle-orm/d1';
 
 export function useDatabase(db: D1Database, config?: DrizzleConfig) {
-  const database = drizzle(db, {
+  return drizzle(db, {
+    ...config,
     schema: {
       recalls: recallsTable,
+      sources: sourcesTable,
+      posts: postsTable,
     },
-    ...config,
   });
-  return database;
 }
 
 export type Database = ReturnType<typeof useDatabase>;
--- src/db/schemas.sql.ts
+++ src/db/schemas.sql.ts
@@ -1,19 +1,82 @@
-import { createdAt, deletedAt, updatedAt } from '@/db/extra';
+import { createdAt, deletedAt, id, updatedAt } from '@/db/extra';
+import { relations } from 'drizzle-orm';
 import { integer, sqliteTable, text } from 'drizzle-orm/sqlite-core';
 
 export const recallsTable = sqliteTable('recalls', {
-  id: integer().notNull().primaryKey({ autoIncrement: true }),
-  date: integer({ mode: 'timestamp' }).$type<Date>(),
+  ...id,
+  sourceId: integer()
+    .references(() => sourcesTable.id)
+    .notNull(),
   linkHref: text().notNull().unique(),
   linkText: text().notNull(),
   product: text().notNull(),
   category: text().notNull(),
   reason: text().notNull(),
   company: text().notNull(),
+  date: integer({ mode: 'timestamp' }).$type<Date>(),
   ...createdAt,
   ...updatedAt,
   ...deletedAt,
 });
 
+export const recallsTableRelations = relations(recallsTable, ({ one, many }) => ({
+  source: one(sourcesTable, {
+    fields: [recallsTable.sourceId],
+    references: [sourcesTable.id],
+  }),
+  posts: many(postsTable),
+}));
+
 export type NewRecall = (typeof recallsTable)['$inferInsert'];
 export type Recall = (typeof recallsTable)['$inferSelect'];
+
+export const sourcesTable = sqliteTable('sources', {
+  ...id,
+  /**
+   * ISO 3166-1 alpha-2 country code and organizational abbreviation.
+   * Example: US-FDA
+   * */
+  key: text().notNull().unique(), // US-FDA
+  /**
+   * Full name of source.
+   * Example: U.S. Food and Drug Administration
+   * */
+  name: text().notNull(),
+  ...createdAt,
+  ...updatedAt,
+  ...deletedAt,
+});
+
+export const sourcesTableRelations = relations(sourcesTable, ({ many }) => ({
+  recalls: many(recallsTable),
+}));
+
+export type NewSource = (typeof sourcesTable)['$inferInsert'];
+export type Source = (typeof sourcesTable)['$inferSelect'];
+
+export const postsTable = sqliteTable('posts', {
+  ...id,
+  title: text().notNull(),
+  content: text().notNull(),
+  uri: text().notNull(),
+  cid: text().notNull(),
+  metadata: text({ mode: 'json' }),
+  embeds: text({ mode: 'json' }),
+  raw: text({ mode: 'json' }).notNull(),
+  recallId: integer()
+    .references(() => recallsTable.id)
+    .notNull(),
+  ...createdAt,
+  ...updatedAt,
+  ...deletedAt,
+});
+
+export const postsTableRelations = relations(postsTable, ({ one }) => ({
+  recall: one(recallsTable, {
+    fields: [postsTable.recallId],
+    references: [recallsTable.id],
+  }),
+}));
+
+export type NewPost = (typeof postsTable)['$inferInsert'];
+export type Post = (typeof postsTable)['$inferSelect'];
