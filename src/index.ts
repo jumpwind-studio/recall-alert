f9253940b579ba291b3214fa368d24b8d270f7e8
timredd <timredd13@gmail.com>
2024-11-26 10:43:19 -0500

    Add way to check instance status

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,30 @@ export { FdaWorkflow } from '@/workflows';
 
 // Export worker handlers
 export default {
-  fetch: async (event, env) => {
-    return new Response('OK', { status: 200 });
+  fetch: async (req, env) => {
+    const url = new URL(req.url);
+
+    if (url.pathname.startsWith('/favicon')) {
+      return Response.json({}, { status: 404 });
+    }
+
+    // Get the status of an existing instance, if provided
+    const id = url.searchParams.get('instanceId') || url.searchParams.get('id');
+    if (id) {
+      const instance = await env.WORKFLOW_FDA.get(id);
+
+      return Response.json({
+        status: await instance.status(),
+      });
+    }
+
+    // Spawn a new instance and return the ID and status
+    const instance = await env.WORKFLOW_FDA.create();
+
+    return Response.json({
+      id: instance.id,
+      details: await instance.status(),
+    });
   },
   scheduled: async (_event, env, ctx) => {
     ctx.waitUntil(env.WORKFLOW_FDA.create());
