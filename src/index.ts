a8297465205144ce45d83af220e8cf93c49db539
timredd <timredd13@gmail.com>
2024-12-04 13:22:22 -0500

    Swap console.log to console.debug

--- src/index.ts
+++ src/index.ts
@@ -1,49 +1,37 @@
 import { hc } from 'hono/client';
 import api, { type API } from './server';
 
-export { FdaWorkflow } from '@/workflows';
-
-function createClient() {
-  return hc<API>('http://localhost:8787').api;
-}
-
-export type Client = ReturnType<typeof createClient>;
-
 export default {
   fetch: api.fetch,
   scheduled: async () => {
-    const api = createClient();
+    const { api } = hc<API>('http://localhost:8787');
 
-    const recallsResponse = await api.recalls.$post({
-      json: { source: 'US-FDA' },
-    });
+    const recallsResponse = await api.recalls.$post({ json: { source: 'US-FDA' } });
     if (!recallsResponse.ok) {
-      console.log(`Failed to fetch recalls: ${await recallsResponse.text()}`);
+      console.debug(`Failed to fetch recalls:\n${await recallsResponse.text()}`);
       return undefined;
     }
     if (recallsResponse.status === 200) {
-      console.log('No new recalls found');
+      console.debug('No new recalls found');
       return undefined;
     }
 
     const { data: recalls } = await recallsResponse.json();
     const recallIds = recalls.map(({ id }) => id);
-    console.log(`Fetched ${recallIds.length} recalls: ${recallIds.join(', ')}`);
+    console.debug(`Fetched ${recallIds.length} recalls`);
 
-    const postsResponse = await api.posts.$post({
-      json: { ids: recallIds },
-    });
+    const postsResponse = await api.posts.$post({ json: { ids: recallIds } });
     if (!postsResponse.ok) {
-      console.log(`Failed to create posts: ${await postsResponse.text()}`);
+      console.debug(`Failed to create posts:\n${await postsResponse.text()}`);
       return undefined;
     }
     if (postsResponse.status === 200) {
-      console.log('No new posts created');
+      console.debug('No new posts created');
       return undefined;
     }
 
     const { data: posts } = await postsResponse.json();
 
-    console.log(JSON.stringify({ recalls, posts }, null, 2));
+    console.debug(JSON.stringify({ recalls, posts }, null, 2));
   },
 } satisfies ExportedHandler<Env>;
