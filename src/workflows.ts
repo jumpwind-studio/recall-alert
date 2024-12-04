2ef92202a09ffaf4144c0b8d3397a64c5039eafe
timredd <timredd13@gmail.com>
2024-12-04 13:09:06 -0500

    Update workflow (to be deleted)

--- src/workflows.ts
+++ src/workflows.ts
@@ -1,120 +1,47 @@
 import { WorkflowEntrypoint, type WorkflowEvent, type WorkflowStep } from 'cloudflare:workers';
-import { createFdaClient } from '@/api/fda';
-import { useDatabase } from '@/db/client';
-import { type NewPost, NewPostSchema, postsTable, recallsTable, sourcesTable } from '@/db/schemas.sql';
-import { createBskyBot } from '@/integrations/bsky/bot';
-import { eq } from 'drizzle-orm';
-import type { BatchItem } from 'drizzle-orm/batch';
-import * as v from 'valibot';
-
-const NOOP = undefined;
+import { hc } from 'hono/client';
+import type { API } from './server';
 
 export class FdaWorkflow extends WorkflowEntrypoint<Env> {
   async run(_: WorkflowEvent<Params>, step: WorkflowStep) {
-    const { data: recallData } = await step.do('fetch live recall data', async () => {
-      return createFdaClient()
-        .list()
-        .then((res) => res.unwrap('Failed to fetch recall data'));
-    });
+    const { recalls } = await step.do('fetch live recall data', async () => {
+      const api = createClient();
 
-    const sourceId = await step.do('fetch source id', async () => {
-      const [row] = await useDatabase(this.env.DB)
-        .select({ sourceId: sourcesTable.id })
-        .from(sourcesTable)
-        .where(eq(sourcesTable.key, 'US-FDA'))
-        .limit(1);
-      return row.sourceId;
-    });
-
-    const recalls = await step.do('persist recall data', async () => {
-      return useDatabase(this.env.DB)
-        .insert(recallsTable)
-        .values(recallData.map((recall) => ({ ...recall, sourceId })))
-        .onConflictDoNothing({ target: recallsTable.url })
-        .returning();
-    });
-
-    if (!recalls || recalls.length === 0) {
-      console.log('No new recalls found');
-      return NOOP;
-    }
-
-    const postData = await step.do('create posts', async () => {
-      return recalls.map((recall) => {
-        const postData = {
-          text: `🚨 RECALL ALERT (${recall.category}) 🚨
-
-Product: ${recall.product}
-Company: ${recall.company}
-Reason: ${recall.reason}
-
-Stay safe and informed! 🛡
-For more details, see below! 👇`,
-          langs: ['en-US'],
-          embed: {
-            $type: 'app.bsky.embed.external',
-            external: {
-              uri: recall.url,
-              title: `${recall.brand} announce(s) recall!`,
-              description: recall.reason,
-            },
-          },
-        };
-
-        return v.parse(NewPostSchema, {
-          recallId: recall.id,
-          title: `${recall.brand} announce(s) recall!`,
-          content: postData.text,
-          raw: JSON.stringify({ postData }),
-          embeds: JSON.stringify(postData.embed),
-          uri: '',
-          cid: '',
-        }) as NewPost;
+      const res = await api.recalls.$post({
+        json: {
+          source: 'US-FDA',
+        },
       });
-    });
-
-    const posts = await step.do('create posts', async () => {
-      return useDatabase(this.env.DB)
-        .insert(postsTable)
-        .values(postData)
-        .onConflictDoNothing({ target: [postsTable.uri] })
-        .returning({
-          id: postsTable.id,
-          recallId: postsTable.recallId,
-          content: postsTable.content,
+      if (!res.ok) {
+        return JSON.stringify({
+          status: `Failed to fetch recalls: ${res.statusText}`,
+          body: await res.text(),
         });
+      }
+
+      return res.json();
     });
 
-    const publishedPosts = await step.do('post recalls', async () => {
-      const bot = createBskyBot({
-        identifier: this.env.BSKY_USERNAME,
-        password: this.env.BSKY_PASSWORD,
+    await step.do('create posts', async () => {
+      if (recalls.length === 0) {
+        return 'No recalls found';
+      }
+
+      const api = createClient();
+
+      const res = await api.posts.$post({
+        json: {
+          ids: recalls.map(({ id }) => id),
+        },
       });
+      if (!res.ok) {
+        return JSON.stringify({
+          status: `Failed to create posts: ${res.statusText}`,
+          body: await res.text(),
+        });
+      }
 
-      return Promise.all(
-        posts.map((post) => {
-          return step.do(`post ${post.id}`, async () => {
-            const bskyData = await bot.post(post.content);
-
-            return {
-              ...bskyData,
-              id: post.id,
-            };
-          });
-        }),
-      );
-    });
-
-    await step.do('update posts', async () => {
-      const db = useDatabase(this.env.DB);
-
-      await db.batch(
-        await Promise.all(
-          publishedPosts.map(({ id, ...publishedPost }) =>
-            db.update(postsTable).set(publishedPost).where(eq(postsTable.id, id)),
-          ) as unknown as [BatchItem<'sqlite'>, ...BatchItem<'sqlite'>[]],
-        ),
-      );
+      return res.json();
     });
   }
 }
